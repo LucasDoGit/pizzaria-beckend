b5caa1bb333695358bfefbb14222359d3bdfe0c6
lucastimoteo <95625902+LucasDoGit@users.noreply.github.com>
2025-02-21 01:09:15 -0300

    chore(deploy): ajustando pasta dist para deploy

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,9 @@
 import express, { Request, Response, NextFunction } from 'express'
 import 'express-async-errors'
-import cors from 'cors';
+import cors from 'cors'
+import path from 'path'
 
-
-import { router } from './routes'
-
+import { router } from './routes';
 import fileUpload from 'express-fileupload';
 
 const app = express();
@@ -16,10 +15,10 @@ app.use(fileUpload({
 }))
 app.use(router);
 
-// app.use(
-//     '/files',
-//     express.static(path.resolve(__dirname, '..', 'tmp'))
-// )
+app.use(
+    '/files',
+    express.static(path.resolve(__dirname, '..', 'tmp'))
+);
 
 app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
     if(err instanceof Error){
@@ -29,7 +28,8 @@ app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
     }
 
     return res.status(500).json({
-        status: 'error', 
+        status: 'error',
+        message: 'Internal server error.'
     })
 })
 
